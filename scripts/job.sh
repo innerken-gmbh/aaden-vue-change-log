8055b7b81130f119b4cd83f44f8715fe9e661f7b
Haodong <juhaodong@gmail.com>
2021-03-09 10:49:26 +0100

    [GITHUB CI] Update changelog

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,7 +1,7 @@
 LOG=$(git log --pretty=format:"'%h - %s'")
 echo $LOG
 echo $LOG >>CHANGELOG.md
-node scripts/save-change-log.js $LOG
+node save-change-log.js $LOG
 git config user.name Haodong Ju
 git config user.email juhaodong@gmail.com
 git add .
