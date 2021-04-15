956b2ad0cf27fc8fa6ed20b32f7da9fbc68b541f
alfred-gorski <dzhongzhu@icloud.com>
2021-04-15 16:12:55 +0200

    [Changed]: log 的预处理 extract 到计算属性中

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@
 
         <!-- 设置 key 为 project.name，假设 project 下条目名字都不一样 -->
         <v-card-text v-for="project in version.projects" :key="project.name">
-					<v-card-subtitle> 项目名：[ {{ project.name }} ] </v-card-subtitle>
+          <v-card-subtitle> 项目名：[ {{ project.name }} ] </v-card-subtitle>
           <v-treeview dense :items="project.changeLogs"></v-treeview>
         </v-card-text>
       </v-card>
@@ -21,56 +21,60 @@
 <script>
 export default {
   name: "HelloWorld",
-
   data: () => ({
-    log: require("@/assets/changelog/changelog.json")
-      .filter((v) => {
-        return (
-          v.projects[0].name !== undefined && v.projects[0].name.length !== 0
-        );
-      })
-      .map((v) => {
-        v.projects = v.projects
-          .filter((p) => {
-            return p.changeLogs !== undefined && p.changeLogs.length !== 0;
-          })
-          .map((p) => {
-            var index = 0;
-            p.changeLogs = p.changeLogs
-              .filter((l) => {
-                return l.log !== undefined && l.log.length !== 0;
-              })
-              .map((l) => {
-                return {
-                  id: index++,
-                  name: l.type,
-                  children: l.log.map((l) => {
-                    return {
-                      id: index++,
-                      name: "-- " + l.message,
-                    };
-                  }),
-                };
-              });
-            return p;
-          });
-        return v;
-      })
-      .sort((left, right) => {
-        left = left.version.split(".");
-        right = right.version.split(".");
-        let n = Math.max(left.length, right.length);
-        for (let i = 0; i < n; i++) {
-          let code1 = left[i] === undefined ? 0 : parseInt(left[i]);
-          let code2 = right[i] === undefined ? 0 : parseInt(right[i]);
-          if (code1 > code2) {
-            return -1;
-          } else if (code1 < code2) {
-            return 1;
-          }
-        }
-        return 0;
-      }),
+    rawlog: require("@/assets/changelog/changelog.json"),
   }),
+  computed: {
+    log: function () {
+      return this.rawlog
+        .filter((v) => {
+          return (
+            v.projects[0].name !== undefined && v.projects[0].name.length !== 0
+          );
+        })
+        .map((v) => {
+          v.projects = v.projects
+            .filter((p) => {
+              return p.changeLogs !== undefined && p.changeLogs.length !== 0;
+            })
+            .map((p) => {
+              var index = 0;
+              p.changeLogs = p.changeLogs
+                .filter((l) => {
+                  return l.log !== undefined && l.log.length !== 0;
+                })
+                .map((l) => {
+                  return {
+                    id: index++,
+                    name: l.type,
+                    children: l.log.map((l) => {
+                      return {
+                        id: index++,
+                        name: "-- " + l.message,
+                      };
+                    }),
+                  };
+                });
+              return p;
+            });
+          return v;
+        })
+        .sort((left, right) => {
+          left = left.version.split(".");
+          right = right.version.split(".");
+          let n = Math.max(left.length, right.length);
+          for (let i = 0; i < n; i++) {
+            let code1 = left[i] === undefined ? 0 : parseInt(left[i]);
+            let code2 = right[i] === undefined ? 0 : parseInt(right[i]);
+            if (code1 > code2) {
+              return -1;
+            } else if (code1 < code2) {
+              return 1;
+            }
+          }
+          return 0;
+        });
+    },
+  },
 };
 </script>
